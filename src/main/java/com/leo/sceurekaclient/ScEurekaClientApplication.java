30f84c67e3bdb22648d5803902c3754b2e1213a2
wangly1004 <wangliyingwad@163.com>
2018-01-16 20:17:29 +0800

    eureka client init

---
+++ src/main/java/com/leo/sceurekaclient/ScEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.leo.sceurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScEurekaClientApplication.class, args);
+	}
+}
