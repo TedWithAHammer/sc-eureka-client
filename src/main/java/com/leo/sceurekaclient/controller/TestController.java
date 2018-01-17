9cfcc7c1d5fe36a8d7835d17fbf19c67d6bc4b7a
wangly1004 <wangliyingwad@163.com>
2018-01-17 20:16:41 +0800

    1.config change

--- src/main/java/com/leo/sceurekaclient/controller/TestController.java
+++ src/main/java/com/leo/sceurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.leo.sceurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.leo.sceurekaclient.BaseResponse;
@@ -11,8 +12,12 @@ import com.leo.sceurekaclient.RestResponse;
  */
 @RestController
 public class TestController {
+
+//    @Value("${eureka.client.service-url.defaultZone}")
+//    private String url;
+
     @GetMapping("/test")
     public BaseResponse test() {
-        return new RestResponse<String>("this is a test");
+        return new RestResponse<>("this is a test" );
     }
 }
