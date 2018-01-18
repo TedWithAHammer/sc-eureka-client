9e3e997dcdc1914dd30cb8500b6cf32f11282c5b
wangly1004 <wangliyingwad@163.com>
2018-01-18 16:45:04 +0800

    1.invalid profile 2.requestmapping on testcontroller

--- src/main/java/com/leo/sceurekaclient/ScEurekaClientApplication.java
+++ src/main/java/com/leo/sceurekaclient/ScEurekaClientApplication.java
@@ -3,7 +3,6 @@ package com.leo.sceurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +11,5 @@ public class ScEurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScEurekaClientApplication.class, args);
     }
+
 }
--- src/main/java/com/leo/sceurekaclient/controller/TestController.java
+++ src/main/java/com/leo/sceurekaclient/controller/TestController.java
@@ -2,6 +2,7 @@ package com.leo.sceurekaclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.leo.sceurekaclient.BaseResponse;
 import com.leo.sceurekaclient.RestResponse;
@@ -11,12 +12,13 @@ import com.leo.sceurekaclient.RestResponse;
  * Description:
  */
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
 //    @Value("${eureka.client.service-url.defaultZone}")
 //    private String url;
 
-    @GetMapping("/test")
+    @GetMapping("/test-string")
     public BaseResponse test() {
         return new RestResponse<>("this is a test" );
     }
